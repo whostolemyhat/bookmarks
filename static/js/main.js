bfb063ebd34a02904f0163b08d45e32ce10fa84c
James Tease <jamestease@gmail.com>
2012-09-03 16:50:27 +0100

    Started popup/ajax etc

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,12 @@
+$(document).ready(function() {
+    $('.add-bookmark').hide();
+    $('#add-new').click(function() {
+        $('.add-bookmark').slideToggle();
+    });
 
+    $('.delete-bookmark').click(function() {
+        $('<div id="popup" />').appendTo('body');
+        $('#popup').load($(this).attr('href'));
+        return false;
+    });
+});
